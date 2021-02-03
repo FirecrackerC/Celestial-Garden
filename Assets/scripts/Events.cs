7c73dbf6cee158132f3df164cef2c05b173b74e6
kingspin239 <rubenjoossen@gmail.com>
2021-02-03 15:29:53 +0100

    file added
    added the celestial garden game

---
+++ Assets/scripts/Events.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class Events : MonoBehaviour
+{
+    public void OnPress(Hand hand)
+    {
+        Application.Quit();
+    }
+
+    public void OnCustomButtonPress()
+    {
+        Debug.Log("We pushed our custom button!");
+    }
+}
---
+++ Assets/scripts/seed2.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class seed2 : MonoBehaviour
+{
+    public Transform spawnPoint;
+    public GameObject goal;
+    public Rigidbody rb;
+    float speed = 1f;
+    Vector3 direction;
+    public GameObject root;
+    public AudioSource correct;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = spawnPoint.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position == spawnPoint.position)
+        {
+            transform.position = spawnPoint.position;
+            rb.velocity = new Vector3(0, 0, 0);
+        }
+        else
+        {
+            direction = transform.position - goal.transform.position;
+            rb.velocity = direction.normalized * -speed;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    { 
+        if (collision.gameObject.tag == "obstacle")
+        {
+            transform.position = spawnPoint.position;
+            rb.velocity = new Vector3(0, 0, 0);
+        }
+        if (collision.gameObject.tag == "Seed2")
+        {
+            Debug.Log("Hit");
+            correct.Play();
+            Destroy(root);
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
